06afbddf68db1259abf5412daf6c8c9a2995f98b
kjkandrea <kjkandrea@gmail.com>
2023-04-24 12:56:45 +0900

    feat: Proxy handler 검증 규칙 추가

--- src/reservation/handler.ts
+++ src/reservation/handler.ts
@@ -6,6 +6,14 @@ export const reserveHandler: ProxyHandler<ReserveCount> = {
       throw new Error('숫자를 입력해주세요.');
     }
 
+    if (!Number.isInteger(value)) {
+      throw new Error('1 단위의 정수를 입력해주세요.');
+    }
+
+    if (0 > value) {
+      throw new Error('0 이상으로 입력해주세요.');
+    }
+
     target[key] = value;
     return true;
   },
