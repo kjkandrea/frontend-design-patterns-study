5e320d2bb7ccb90778221229707dbf4167db329a
kjkandrea <kjkandrea@gmail.com>
2023-05-19 11:03:34 +0900

    feat: puppy array cash 로직 추가

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log('Hello, world!');
+const puppyRepository: {
+  array: Puppy[];
+} = {
+  array: [],
+};
+
+const puppyService = {
+  addPuppyInArray() {
+    const puppy: Puppy = new Puppy('K', 'Welsh corgi', 3);
+
+    puppyRepository.array.push(puppy);
+  },
+};
+
+class Puppy {
+  id: string;
+  breed: string;
+  age: number;
+
+  constructor(id: string, breed: string, age: number) {
+    this.id = id;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById('execute-add-puppy-in-array-click-trigger')
+    ?.addEventListener('click', () => puppyService.addPuppyInArray());
+});
