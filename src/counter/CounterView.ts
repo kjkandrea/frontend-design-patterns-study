43fa1ee1e3f6a5a75f0045e8f3159b70893c8c44
kjkandrea <kjkandrea@gmail.com>
2023-06-01 16:19:33 +0900

    feat: CounterView render 로직 추가

--- src/counter/CounterView.ts
+++ src/counter/CounterView.ts
@@ -1,10 +1,25 @@
-import {View} from 'src/Strategy';
+import {View} from '../Strategy';
 
 export class CounterView implements View {
   public render(): HTMLElement {
     const rootEl = document.createElement('div');
-    rootEl.textContent = 'Hello, SOC!';
 
+    const countTextEl = document.createElement('p');
+    countTextEl.textContent = String(0);
+
+    const [incrementButtonEl, decrementButtonEl] = [
+      this.createClickableButton('increment'),
+      this.createClickableButton('decrement'),
+    ];
+
+    rootEl.append(incrementButtonEl, countTextEl, decrementButtonEl);
     return rootEl;
+  }
+
+  private createClickableButton(text: string): HTMLButtonElement {
+    const buttonEl = document.createElement('button');
+    buttonEl.textContent = text;
+    buttonEl.type = 'button';
+    return buttonEl;
   }
 }
